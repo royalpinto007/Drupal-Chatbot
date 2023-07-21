60e573e0a49913d484710b8c9e2e7a0b5f645250
royalpinto007 <royalpinto007@gmail.com>
2023-07-21 14:34:03 +0530

    feat: bot message logic with OpenAI

--- modules/chatbot/js/chatbot.js
+++ modules/chatbot/js/chatbot.js
@@ -12,7 +12,7 @@
 			chatbotIcon.style.display = "none";
 		} else {
 			chatWindow.style.display = "none";
-			chatbotIcon.style.display = 'block';
+			chatbotIcon.style.display = "block";
 		}
 	});
 
@@ -23,6 +23,67 @@
 			userInput.value = "";
 		}
 	}
+
+	// Open AI
+	// Comment out the above sendMessage function and 
+	// uncomment the below sendMessage function to use Open AI
+	// async function sendMessage() {
+	// 	const userMessage = userInput.value;
+	// 	if (userMessage) {
+	// 		appendMessage(userMessage, "user-message");
+	// 		userInput.value = "";
+
+	// 		const response = await fetch(
+	// 			"https://api.openai.com/v1/chat/completions",
+	// 			{
+	// 				method: "POST",
+	// 				headers: {
+	// 					Authorization:
+	// 						"Bearer API_KEY_HERE",
+	// 					"Content-Type": "application/json",
+	// 				},
+	// 				body: JSON.stringify({
+	// 					model: "gpt-3.5-turbo",
+	// 					messages: [
+	// 						{
+	// 							role: "system",
+	// 							content:
+	// 								"You can start the conversation with a system message.",
+	// 						},
+	// 						{
+	// 							role: "user",
+	// 							content: userMessage,
+	// 						},
+	// 					],
+	// 					max_tokens: 50,
+	// 					temperature: 0.7,
+	// 					n: 1,
+	// 				}),
+	// 			}
+	// 		);
+
+	// 		// if (response.status === 429) {
+	// 		// 	// If rate limit exceeded, retry after a delay
+	// 		// 	const retryAfter = parseInt(response.headers.get("Retry-After"));
+	// 		// 	await new Promise((resolve) => setTimeout(resolve, retryAfter * 1000));
+	// 		// 	sendMessage(); // Retry the API request
+	// 		// 	return;
+	// 		// }
+
+	// 		console.log(response);
+
+	// 		const data = await response.json();
+	// 		console.log(data); 
+
+	// 		const aiMessage =
+	// 			data.choices && data.choices.length > 0
+	// 				? data.choices[0].message.content.trim()
+	// 				: "";
+	// 		console.log(aiMessage);
+
+	// 		appendMessage(aiMessage, "ai-message");
+	// 	}
+	// }
 
 	sendBtn.addEventListener("click", sendMessage);
 
@@ -37,6 +98,7 @@
 		const messageElement = document.createElement("div");
 		messageElement.classList.add(className);
 		messageElement.innerText = message;
+		messageElement.classList.add("bot-message");
 		chatlog.appendChild(messageElement);
 		chatlog.scrollTop = chatlog.scrollHeight;
 	}
